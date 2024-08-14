164d6e6364e5bb31b8a0905314d12881ca961884
SimonPimonEdgar <167124993+SimonPimonEdgar@users.noreply.github.com>
2024-08-14 15:49:11 +0200

    Delete Task_B directory

--- Task_B/B_2/Solution/include/read_file.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <string>
-
-const char* getDefaultDelimitersAndStopChars();
-
-bool isDelimiterOrStopChar ( char c );
-
-std::vector<std::string> readTextFromFile( std::string fileName );

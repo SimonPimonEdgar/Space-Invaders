a2408564c5661a1dbc5ee21689b57c7b7df46ce3
Simon Hemkentokrax <shemkent@uni-muenster.de>
2024-08-16 18:52:41 +0200

    Model Classes

--- Final_Task/src/model_simulator_game.h
+++ Final_Task/src/model_simulator_game.h
@@ -14,6 +14,58 @@ private:
     int x, y, height; // player's coordinates and height
 };
 
+class Alien {
+public:
+    Alien(AlienVar var, int x, int y); // constructor that takes in the variant and the initial x and y coordinates of the alien
+    AlienVar getVar();
+    int getX();
+    int getY();
+    int getH();
+    bool getDead();
+    void setX(int a);
+    void setY(int a);
+    void setH(int a);
+    void setDead(bool a);
+
+private:
+    int x, y; // coordinates of the alien
+    bool dead = false; // bool wether or not the alien got hit
+    AlienVar var; // variant of the alien
+};
+
+enum class AlienVar{1, 2, 3};
+
+
+class Shot {
+public:
+    Shot(int x, int y, bool dir); // contructor that takes in the direction and the initial x and y coordinates of the shot
+    int getX();
+    int getY();
+    bool getDir();
+    void setX(int a);
+    void setY(int a);
+
+private:
+    int x, y; // coordinates of the shot
+    bool dir; // direction of the shot (Upwards = True, Downwards = False)
+};
+
+class PowerUp{
+public:
+    PowerUp(int x, int y, PowerUpVar var); // constructor that takes in the variant and the initial x and y coordinates of the power up
+    int getX();
+    int getY();
+    int getVar();
+    void setX();
+    void setY();
+
+private:
+    int x, y; // coordinates of the power up
+    PowerUpVar var; // Variant of the power up
+};
+
+enum class PowerUpVar{1, 2, 3}; 
+
 class GameModel : public Observable { // Game class inherits from Observable class
 public:
     GameModel(); // constructor
